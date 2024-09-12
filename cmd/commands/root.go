5fb2f02038d4a6bfcd762df0dbdadbe0b2d14e84
nullswan <pro@nullswan.io>
2024-09-12 18:22:54 +0200

    style(commands): combine var declaration into a single line

--- cmd/commands/root.go
+++ cmd/commands/root.go
@@ -8,9 +8,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	apiKey string
-)
+var apiKey string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
